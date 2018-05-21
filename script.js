96787baf73c662bd4b4cdb5415313c214c7b2ff4
Brian Chitester <yagoogaly@gmail.com>
2018-05-20 21:48:45 -0400

    edit mode somewhat working

--- script.js
+++ script.js
@@ -1,71 +1,81 @@
+
+
+const padsConfig = {
+  D: {
+    audio: 'https://demo.agektmr.com/drumpad/samples/Kit3/hihat.wav',
+    color: 'green',
+    text: 'hat',
+  },
+  F: {
+    audio: 'https://demo.agektmr.com/drumpad/samples/Kit3/snare.wav',
+    color: 'green',
+    text: 'snare',
+  },
+  J: {
+    audio: 'https://demo.agektmr.com/drumpad/samples/Kit3/kick.wav',
+    color: 'green',
+    text: 'kick',
+  },
+  K: {
+    audio: 'https://demo.agektmr.com/drumpad/samples/Kit3/tom3.wav',
+    color: 'green',
+    text: 'low tom',
+  },
+  U: {
+    audio: 'https://demo.agektmr.com/drumpad/samples/Kit8/hihat.wav',
+    color: 'green',
+    text: 'snap',
+  },
+  I: {
+    audio: 'https://demo.agektmr.com/drumpad/samples/Kit8/snare.wav',
+    color: 'green',
+    text: 'clap',
+  },
+  V: {
+    audio: 'https://demo.agektmr.com/drumpad/samples/LINN/snare.wav',
+    color: 'green',
+    text: 'snare2',
+  },
+  W: {
+    audio: 'https://demo.agektmr.com/drumpad/samples/CR78/tom1.wav',
+    color: 'green',
+    text: 'boop',
+  },
+  E: {
+    audio: 'https://demo.agektmr.com/drumpad/samples/CR78/tom2.wav',
+    color: 'green',
+    text: 'boop',
+  },
+  R: {
+    audio: 'https://demo.agektmr.com/drumpad/samples/CR78/tom3.wav',
+    color: 'green',
+    text: 'boop',
+  },
+}
+
+const keys = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+let editMode = false;
+
 $(document).ready(function() {
   
-  const loadAndPlay = (pad) => {
-   pad.load();
-   pad.play();
-  }
 
-  const padsConfig = {
-    D: {
-      audio: 'https://demo.agektmr.com/drumpad/samples/Kit3/hihat.wav',
-      color: 'green',
-      text: 'hat',
-    },
-    F: {
-      audio: 'https://demo.agektmr.com/drumpad/samples/Kit3/snare.wav',
-      color: 'green',
-      text: 'snare',
-    },
-    J: {
-      audio: 'https://demo.agektmr.com/drumpad/samples/Kit3/kick.wav',
-      color: 'green',
-      text: 'kick',
-    },
-    K: {
-      audio: 'https://demo.agektmr.com/drumpad/samples/Kit3/tom3.wav',
-      color: 'green',
-      text: 'low tom',
-    },
-    U: {
-      audio: 'https://demo.agektmr.com/drumpad/samples/Kit8/hihat.wav',
-      color: 'green',
-      text: 'snap',
-    },
-    I: {
-      audio: 'https://demo.agektmr.com/drumpad/samples/Kit8/snare.wav',
-      color: 'green',
-      text: 'clap',
-    },
-    V: {
-      audio: 'https://demo.agektmr.com/drumpad/samples/LINN/snare.wav',
-      color: 'green',
-      text: 'snare2',
-    },
-    W: {
-      audio: 'https://demo.agektmr.com/drumpad/samples/CR78/tom1.wav',
-      color: 'green',
-      text: 'boop',
-    },
-    E: {
-      audio: 'https://demo.agektmr.com/drumpad/samples/CR78/tom2.wav',
-      color: 'green',
-      text: 'boop',
-    },
-    R: {
-      audio: 'https://demo.agektmr.com/drumpad/samples/CR78/tom3.wav',
-      color: 'green',
-      text: 'boop',
-    },
-  }
+  keys.forEach((key) => {
+    $("."+key).mousedown(() => {
+        //open editor;
+        $(".editor").css("display", "block");
+        editMode = true;
+        $("#editorKey").text(key)
+        $("#editorKey").addClass(key)
+        $("#audio").val(padsConfig[key].audio)
+        $("#text").val(padsConfig[key].text)
+        $("#color").val(padsConfig[key].color)
+     });
+  })
 
   const pads = {}
   for (key in padsConfig) {
      let pad = new Audio(padsConfig[key].audio);
-     $("."+key).mousedown(() => {
-        //open editor;
-        $(".editor").css("display", "block");
-
-     });
      $("."+key).css("background-color", padsConfig[key].color);
      $("."+key).text(padsConfig[key].text);
      pads[key] = pad;
@@ -74,19 +84,34 @@ $(document).ready(function() {
   $(window).keydown(function(e) {
     var kc = getKey(e.keyCode);
     if (kc){
-      $("."+kc).addClass("active");
     }
 
-    if (pads[kc]){
+    if (pads[kc] && !editMode){
+      $("."+kc).addClass("active");
       loadAndPlay(pads[kc]);
     }
   });
 
   $(window).keyup(function(e) {
     var kc = getKey(e.keyCode);
-    if (kc){
+    if (kc && !editMode){
       $("."+kc).removeClass("active");
     }
+  });
+
+  $("#editor").submit(function(e) {
+    e.preventDefault();
+    $(".editor").css("display", "none");
+    editMode = false
+    const key = $("#editorKey").text();
+    let audio = $("#audio").val() !== "" ? $("#audio").val() : padsConfig[key].audio;
+    let text = $("#text").val() !== "" ? $("#text").val() : padsConfig[key].text;
+    let color = $("#color").val() !== "" ? $("#color").val() : padsConfig[key].color;
+
+    let pad = new Audio(audio);
+    $("."+key).css("background-color", color);
+    $("."+key).text(text);
+    pads[key] = pad;
   });
 
 });
@@ -100,6 +125,7 @@ const getKey = (keyCode) => {
 
 }
 
-const updatePad = () => {
-
+const loadAndPlay = (pad) => {
+ pad.load();
+ pad.play();
 }
